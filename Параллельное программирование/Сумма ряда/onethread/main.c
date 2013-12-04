8757eec2881b473fc5c311cc32a4ea3713726177
Ринат Ибрагимов <XaoZlo@rambler.ru>
2013-12-04 12:29:40 +0600

    unsigned long long для onethread и openMP

--- Параллельное программирование/Сумма ряда/onethread/main.c
+++ Параллельное программирование/Сумма ряда/onethread/main.c
@@ -3,21 +3,21 @@
 #include <math.h>
 #include <time.h>
 
-double f(long long int n){
- return sin(1/(n*n))/((5 * n - 1)*(5*n - 1));
+static inline double f(unsigned long long int n){
+ return sin(1 / (n * n)) / ((5 * n - 1) * (5 * n - 1));
 }
 
-double sum(long long int N){
+double sum(unsigned long long int N){
  double SUM = 0;
  #pragma loop count min(16)
- for(long long int n = 0; n < N; ++n){
-  SUM += f(n+1);
+ for(unsigned long long int n = 1; n <= N; ++n){
+  SUM += f(n);
  }
  return SUM;
 }
 
 int main(int argc, char *argv[]){
- long long int N = argc > 1 ? atol(argv[1]) : 1000000;
+ unsigned long long int N = argc > 1 ? atol(argv[1]) : 1000000;
 
  printf("sum %lld = ", N);
  clock_t time = clock();
--- Параллельное программирование/Сумма ряда/openMP/main.c
+++ Параллельное программирование/Сумма ряда/openMP/main.c
@@ -3,29 +3,27 @@
 #include <math.h>
 #include <time.h>
 
-double f(long long int n){
- return sin(1/pow(n, 2))/pow(5 * n - 1, 2);
+double f(unsigned long long int n){
+ return sin(1 / (n * n)) / ((5 * n - 1) * (5 * n - 1));
 }
 
-double sum(long long int N){
+double sum(unsigned long long int N){
  double SUM = 0;
  #pragma omp parallel for
- for(long long int n = 0; n < N; ++n){
-  SUM += f(n+1);
+ for(unsigned long long int n = 1; n <= N; ++n){
+  SUM += f(n);
  }
  return SUM;
 }
 
 int main(int argc, char *argv[]){
- long long int N = argc > 1 ? atol(argv[1]) : 10000;
- 
+ unsigned long long int N = argc > 1 ? atol(argv[1]) : 100000;
  printf("sum %lld = ", N);
  clock_t time = clock();
  double SUM;
  SUM = sum(N);
  printf("%lf\n", SUM);
  time = clock() - time;
- printf("evaluation time: %lf\n", (double)time/CLOCKS_PER_SEC);
+ printf("evaluation time: %lf\n", (double)time / CLOCKS_PER_SEC);
  return 0;
 }
-
