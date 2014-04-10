f4c20fc99a84b77878acd8c51658599e7e558fc3
Ринат Ибрагимов <XaoZlo@rambler.ru>
2014-04-10 16:54:44 +0600

    final updates for lab1 launch

--- Параллельное Программирование/Сумма ряда/mpi/start.sh
+++ Параллельное Программирование/Сумма ряда/mpi/start.sh
@@ -2,8 +2,8 @@
 
 ./compile.sh
 
-./launch.sh 1 8 10000
-./launch.sh 2 16 10000
-./launch.sh 4 32 10000
-./launch.sh 8 64 10000
-./launch.sh 16 128 10000
+./launch.sh 1 8 3400000000
+./launch.sh 2 16 3400000000
+./launch.sh 4 32 3400000000
+./launch.sh 8 64 3400000000
+./launch.sh 16 128 3400000000
